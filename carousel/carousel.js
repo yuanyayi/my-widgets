a8ac8e68e8b1ccb9851af534e54b61923b3800f0
YuanYayi <callmecrystal@163.com>
2018-08-01 10:42:46 +0800

    自制简单轮播图组件

---
+++ carousel/carousel.js
@@ -0,0 +1,75 @@
+function Carousel($el, autoplay, delayTime) {
+  let _this = this
+  this.viewport = $el.find('.viewport')
+  this.items = $el.find('.itemlist')
+  this.itemlist = this.items.find('.item')
+  let itemWidth = this.itemlist.eq(0).width()
+  let itemLength = this.itemlist.length
+  this.itemlist.map((index, item) => {
+    $(item).css({ left: index * itemWidth })
+  })
+  // clone first and last
+  this.itemlist.eq(-1).clone().addClass('clone')
+    .insertBefore(this.itemlist.eq(0)).css({ left: -1 * itemWidth })
+  this.itemlist.eq(0).clone().addClass('clone')
+    .insertAfter(this.itemlist.eq(-1)).css({ left: itemLength * itemWidth })
+  // functions 
+  // 基本跳转
+  let delay = delayTime || 1000
+  let current = 0
+
+  this._next = () => {
+    current += 1
+    this.items.animate({ left: -itemWidth * current }, delay)
+    if (current === itemLength) {
+      current = 0
+      this.items.animate({ left: 0 }, 0)
+    }
+  }
+  this._prev = () => {
+    current -= 1
+    this.items.animate({ left: -itemWidth * current }, delay)
+    if (current === -1) {
+      current = itemLength - 1
+      this.items.animate({ left: -itemWidth * current }, 0)
+    }
+  }
+  // DOM-上一下一
+  $el.find('.next').on('click', () => {
+    this._next()
+  })
+  $el.find('.prev').on('click', () => {
+    this._prev()
+  })
+  // 导航跳转
+  this._gotoIndex = (i) => {
+    current = i
+    this.items.stop(true, false).animate({ left: -itemWidth * current }, delay)
+  }
+  // DOM-导航按钮
+  $el.append('<ol class="carousel-nav"></ol>')
+  let nav = $el.find('.carousel-nav')
+  for (let i = 0; i < itemLength; i++) {
+    nav.append(`<i data-i="${i}">${i}</i>`)
+  }
+  nav.on('click', 'i', (ev) => {
+    console.log($(ev.target).data('i'))
+    _this._gotoIndex($(ev.target).data('i'))
+  })
+  this.autoplayTimer = null
+  // 自动播放相关
+  function autorun(autoplay) {
+    if (autoplay !== undefined && !autoplay) return false
+    _this.autoplayTimer = setInterval(()=>{_this._next()}, delay*3.5)
+    _this.itemlist.on('mouseenter', () => {
+      clearInterval(_this.autoplayTimer)
+    })
+    _this.itemlist.on('mouseleave', () => {
+      _this.autoplayTimer = setInterval(()=>{_this._next()}, delay*3.5)
+      _this._next()
+    })
+  }
+  // DOM-自动播放？
+  autorun(autoplay);
+
+}
